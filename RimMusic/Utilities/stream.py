ee46745bbffcf8b0e76e22a11f60cdaafeab5ed5
mocpi07 <96298243+mocpi07@users.noreply.github.com>
2022-02-03 11:46:55 +0700

    Update stream.py

--- RimMusic/Utilities/stream.py
+++ RimMusic/Utilities/stream.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup
 from config import get_queue
 from RimMusic import BOT_USERNAME, db_mem
 from RimMusic.Core.PyTgCalls import Queues
-from RimMusic.Core.PyTgCalls.Yukki import join_stream
+from RimMusic.Core.PyTgCalls.RimMusic import join_stream
 from RimMusic.Database import (add_active_chat, add_active_video_chat,
                             is_active_chat, music_off, music_on)
 from RimMusic.Inline import (audio_markup, audio_markup2, primary_markup,
